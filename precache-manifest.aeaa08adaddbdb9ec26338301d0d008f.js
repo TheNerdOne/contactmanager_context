188893eac08c9f4d82369fc43d4da5effbd64279
pouria <poria9chegeni@gmail.com>
2020-02-23 16:51:32 +0330

    Updates

---
+++ precache-manifest.aeaa08adaddbdb9ec26338301d0d008f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce92298b8f9ef295e2dadc6d6bd281f",
+    "url": "/contactmanager_context/index.html"
+  },
+  {
+    "revision": "d950bf826684431ca3b7",
+    "url": "/contactmanager_context/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2521a43af92768cd8f97",
+    "url": "/contactmanager_context/static/css/main.fe72e0ff.chunk.css"
+  },
+  {
+    "revision": "d950bf826684431ca3b7",
+    "url": "/contactmanager_context/static/js/2.af29477d.chunk.js"
+  },
+  {
+    "revision": "6da181100a4b10949ce791f77892b06b",
+    "url": "/contactmanager_context/static/js/2.af29477d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2521a43af92768cd8f97",
+    "url": "/contactmanager_context/static/js/main.668131a9.chunk.js"
+  },
+  {
+    "revision": "e9bd07d40cfa3ab57477",
+    "url": "/contactmanager_context/static/js/runtime-main.40fe5b33.js"
+  },
+  {
+    "revision": "56f0c7de57fdae6d0a9ddc43448b6dff",
+    "url": "/contactmanager_context/static/media/avatar-circle.56f0c7de.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contactmanager_context/precache-manifest.3cbd691b9e80c632e0494e048c0e18a0.js"
+  "/contactmanager_context/precache-manifest.aeaa08adaddbdb9ec26338301d0d008f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contactmanager_context/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
